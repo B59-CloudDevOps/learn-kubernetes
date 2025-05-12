ee6f207da798fb34558405929bb0d59ec47c115e
CodingManoj <CodingManoj@gmail.com>
2025-05-11 22:05:54 -0400

    K8s Agenda

--- 01-eks-terraform/addOns.tf
+++ 01-eks-terraform/addOns.tf
@@ -4,8 +4,6 @@ resource "aws_eks_addon" "main" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "vpc-cni"
   configuration_values = jsonencode({
-    configuration_values = {
-      "enableNetworkPolicy" = "true"
-    }
+    "enableNetworkPolicy" = "true"
   })
 }
